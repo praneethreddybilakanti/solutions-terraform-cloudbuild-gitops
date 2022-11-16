eca6a2ade84a0284c4748356a901c594816cb8bd
praneethreddybilakanti <praneethreddy8498995302@gmail.com>
2022-11-16 18:06:03 +0000

    Update project IDs and buckets

--- environments/prod/backend.tf
+++ environments/prod/backend.tf
@@ -15,7 +15,7 @@
 
 terraform {
   backend "gcs" {
-    bucket = "PROJECT_ID-tfstate"
+    bucket = "dolphins-kart-dev-apps-tfstate"
     prefix = "env/prod"
   }
 }
--- environments/prod/terraform.tfvars
+++ environments/prod/terraform.tfvars
@@ -1,1 +1,1 @@
-project="PROJECT_ID"
+project="dolphins-kart-dev-apps"
